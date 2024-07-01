eae45cdff3f3e195410cfbe5b5a78177d730bb5a
Malika <65332320+MO1-dot-618@users.noreply.github.com>
2024-07-01 16:25:55 +0100

    Adding urls for html pages and views

--- education/education/urls.py
+++ education/education/urls.py
@@ -15,13 +15,41 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from app import views
-from app.models import CustomUser
+from django.urls import path, include
+from forum.models import CustomUser
+from forum import views
+from django.conf import settings
+from django.conf.urls.static import static
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+
+websocket_urlpatterns = [
+    path('ws/inbox/', InboxConsumer.as_asgi()),
+    path('ws/chat/', ChatConsumer.as_asgi()),
+]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.landing_page, name='landing_page'),
     path('home/', views.home, name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/profile/', views.profile, name='profile'),
+    path('accounts/register/', views.register, name='register'),
+    path('logout/', views.logout_view, name='logout'),
     path('login/', views.custom_login, name='login'),
+    path('threads/', views.threads, name='threads'),
+    path('thread_page/', views.thread_page, name='thread_page'),
+    path('user/', views.user, name='user'),
+    path('upvote/<int:post_id>/', views.upvote_post, name='upvote_post'),
+    path('downvote/<int:post_id>/', views.downvote_post, name='downvote_post'),
+    path('vote_color/<int:post_id>/', views.vote_color, name='vote_color'),
+    path('thread_creation/', views.thread_creation, name='thread_creation'),
+    path('post_creation/', views.post_creation, name='post_creation'),
+    path('delete_post/<int:post_id>/', views.delete_post, name='delete_post'),
+    path('delete_thread/<int:thread_id>/', views.delete_thread, name='delete_thread'),
+    path('edit_post/<int:post_id>/', views.edit_post, name='edit_post'),
+    path('save_post/<int:post_id>/', views.save_post, name='save_post'),
+    path('save_thread/<int:thread_id>/', views.save_thread, name='save_thread'),
+    path('thread_page/<int:thread>/', views.thread_page, name='thread_page'),
+    path('thread_page/<int:post_id>/', views.thread_page, name='thread_page'),
 ]
